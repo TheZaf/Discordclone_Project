31d912b5175463142790032de8b6671584c61490
TheZaf <128605103+TheZaf@users.noreply.github.com>
2024-04-06 12:53:12 +0530

    Added a files

---
+++ script.js
@@ -0,0 +1,37 @@
+function onlinefun(){
+    document.getElementById('midCon').style.display="none";
+    document.getElementById('midCon2').style.display="block";
+    document.getElementById('midCon3').style.display="none";
+    document.getElementById('midCon4').style.display="none";
+    document.getElementById('midCon5').style.display="none";
+}
+function addfriend(){
+    document.getElementById('midCon4').style.display="none";
+    document.getElementById('midCon2').style.display="none";
+    document.getElementById('midCon3').style.display="none";
+    document.getElementById('midCon5').style.display="none";
+    document.getElementById('midCon').style.display="block";
+}
+function block(){
+    document.getElementById('midCon').style.display="none";
+    document.getElementById('midCon2').style.display="none";
+    document.getElementById('midCon4').style.display="none";
+    document.getElementById('midCon5').style.display="none";
+    document.getElementById('midCon3').style.display="block";
+}
+function pending(){
+    document.getElementById('midCon').style.display="none";
+    document.getElementById('midCon2').style.display="none";
+    document.getElementById('midCon3').style.display="none";
+    document.getElementById('midCon5').style.display="none";
+    document.getElementById('midCon4').style.display="block";
+
+}
+function alls(){
+    document.getElementById('midCon').style.display="none";
+    document.getElementById('midCon2').style.display="none";
+    document.getElementById('midCon3').style.display="none";
+    document.getElementById('midCon4').style.display="none";
+    document.getElementById('midCon5').style.display="block";
+
+}
